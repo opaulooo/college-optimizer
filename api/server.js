0690ffd3d0ee6f41b5867adce336627fbcacedb9
opaulooo <o.paulooo.victor@gmail.com>
2022-06-29 21:20:43 -0300

    fix: api port

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const app = express()
-const port = 8100
+const port = 8000
 
 let comissaoRoute = require('./src/routes/routes')
 let bodyParser = require('body-parser');
