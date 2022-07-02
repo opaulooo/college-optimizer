51ee904d9ec2a0d4919434ed50efd34217538764
opaulooo <o.paulooo.victor@gmail.com>
2022-07-02 19:16:50 -0300

    feat: listagem de tarefas

--- app/src/app/tarefas/tarefas.page.ts
+++ app/src/app/tarefas/tarefas.page.ts
@@ -6,7 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['tarefas.page.scss']
 })
 export class TarefasPage {
+  taskName: any = ''; // Entered Text
+  taskList = []; // Array to store tasks
 
-  constructor() {}
+  constructor() { }
 
+  // addTask Function
+  // First we check if the text is entered or not in input box by verifying if length > 0
+  // If length is greater than 0, then only we add taskName to taskList array
+  // After adding we reset the taskName
+  addTask() {
+    if (this.taskName.length > 0) {
+      let task = this.taskName;
+      this.taskList.push(task);
+      this.taskName = '';
+    }
+  }
+  // deleteTask Function
+  // When user clicks the delete task button, this function is called with index i as parameter
+  // Since tasks are added to taskList, we delete the task at index i using splice() array method
+  // This deletes only that task at index i
+  deleteTask(index) {
+    this.taskList.splice(index, 1);
+  }
 }
